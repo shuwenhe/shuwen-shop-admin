cf6f7b7a9f720adb3df76fb6b5be7cca400fc795
shuwen he <645996795@qq.com>
2020-11-07 15:57:52 +0800

    feat: item_cat.sql

---
+++ sql/item_cat.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : shuwenshopadmin
+ Source Server Type    : MySQL
+ Source Server Version : 50647
+ Source Host           : 62.234.11.179:3306
+ Source Schema         : shuwenshopadmin
+
+ Target Server Type    : MySQL
+ Target Server Version : 50647
+ File Encoding         : 65001
+
+ Date: 07/11/2020 15:51:46
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for item_cat
+-- ----------------------------
+DROP TABLE IF EXISTS `item_cat`;
+CREATE TABLE `item_cat` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '类目ID',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父类目ID=0时，代表的是一级类目',
+  `name` varchar(50) DEFAULT NULL COMMENT '类目名称',
+  `status` int(1) DEFAULT '1' COMMENT '状态，可选值：1（正常），2（删除）',
+  `sort_order` int(4) DEFAULT NULL COMMENT '排序序号,表示同级类目的展现次序，如数值相等则按名称次序排列，取值范围：大于零的整数',
+  `is_parent` tinyint(1) DEFAULT '1' COMMENT '该类目是否为父类目，1为true,0为false',
+  `created` datetime DEFAULT NULL COMMENT '创建时间',
+  `updated` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+	KEY `parent_id`(`parent_id`,`status`)USING BTREE,
+	KEY `sort_order` (`sort_order`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品类目';
+
+SET FOREIGN_KEY_CHECKS = 1;
